5e7ee75202bd84448a01672c10a804eac0f4abd2
Sebastian Szuch <98364552+sjszuch@users.noreply.github.com>
2024-03-10 15:40:18 -0400

    Implemented snackbar on add page

--- src/app/components/addpopup/addpopup.component.ts
+++ src/app/components/addpopup/addpopup.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { Inject } from '@angular/core';
+
 
 @Component({
   selector: 'app-addpopup',
@@ -6,5 +10,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./addpopup.component.scss']
 })
 export class AddpopupComponent {
+  constructor(private _snackBar: MatSnackBar,
+    @Inject(MAT_DIALOG_DATA) public id: any,) {}
 
+  openSnackBar() {
+    this._snackBar.open("Employee Added!", "Close", {duration: 3000});
+  }
 }
