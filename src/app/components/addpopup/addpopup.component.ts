39fd85e77920368d940a5e5beaf3d63ca4d8629e
Sebastian Szuch <98364552+sjszuch@users.noreply.github.com>
2024-03-24 19:55:03 -0400

    Functionality to add new employees

--- src/app/components/addpopup/addpopup.component.ts
+++ src/app/components/addpopup/addpopup.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {MatSnackBar} from '@angular/material/snack-bar';
 import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -10,10 +11,48 @@ import { Inject } from '@angular/core';
   styleUrls: ['./addpopup.component.scss']
 })
 export class AddpopupComponent {
-  constructor(private _snackBar: MatSnackBar,
-    @Inject(MAT_DIALOG_DATA) public id: any,) {}
 
-  openSnackBar() {
-    this._snackBar.open("Employee Added!", "Close", {duration: 3000});
+  firstName!: string;
+  lastName!: string;
+  position!: string;
+
+
+  constructor(
+    private http: HttpClient,
+    private _snackBar: MatSnackBar,
+    @Inject(MAT_DIALOG_DATA) public id: any,
+  ) { }
+
+  openSnackBar(message: string) {
+    this._snackBar.open(message, "Close", {duration: 3000});
   }
+
+  // Sumbits a new employee. Similar to adding comments.
+  submitEmployee() {
+    const data = {
+      first_name: this.firstName,
+      last_name: this.lastName,
+      position: this.position,
+      id: this.id
+    };
+
+    const apiUrl = 'https://ppu-senior-project.onrender.com/api/newentry';
+
+    this.http.post(apiUrl, data)
+      .subscribe({
+        next: (response: any) => {
+          console.log('API Response:', response);
+          this.openSnackBar('Employee added successfully');
+        },
+        error: (error: any) => {
+          console.error('API Error:', error);
+          this.openSnackBar('Failed to add employee');
+        }
+      });
+
+      // Refreshes the page to show the new employee
+      setTimeout(() => {
+        window.location.reload();
+      }, 1000);
+    }
 }
