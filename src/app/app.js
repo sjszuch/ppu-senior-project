8165fe49245f1a367be43a6788fa9748465f8a4e
Sebastian Szuch <98364552+sjszuch@users.noreply.github.com>
2024-03-24 19:08:22 -0400

    Added post request endpoint

--- src/app/app.js
+++ src/app/app.js
@@ -3,11 +3,15 @@ const mysql = require('mysql');
 const cors = require('cors');
 const pg = require('pg');
 const postgres = require('postgres');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 
 // PostgreSQL connection
@@ -39,11 +43,18 @@ app.get('/api/data', async (req, res) => {
 
 // NEW ENTRY
 app.post('/api/newentry', async (req, res) => {
-  const query = 'INSERT INTO employees (first_name, last_name, position) VALUES ($1, $2, $3)';
-  const data = await pool.query(query, [req.body.first_name, req.body.last_name, req.body.position]);
-  res.json(data);
-});
+  try {
+    const { first_name, last_name, position, id } = req.body;
 
+    const query = 'INSERT INTO employees (first_name, last_name, position, id) VALUES ($1, $2, $3, $4)';
+    const data = await pool.query(query, [first_name, last_name, position, id]);
+
+    res.json({ success: true, message: 'New entry added successfully', data });
+  } catch (error) {
+    console.error('Error adding new entry:', error);
+    res.status(500).json({ error: 'Error' });
+  }
+});
 
 
 
